73c6506cb1cdf4443fdd8ab34d2aa71f6ed93845
tsvarun05 <sbhatvarun05@gmail.com>
2026-04-09 21:39:17 +0530

    Delete BACKEND directory

--- BACKEND/src/main/java/com/neurox/repository/ResponseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.neurox.repository;
-
-import com.neurox.entity.Response;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import java.util.List;
-
-public interface ResponseRepository extends MongoRepository<Response, String> {
-    List<Response> findByUserId(String userId);
-}
